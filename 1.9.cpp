e4aeda5ff433a85860bedc7876adc37f50e0de07
realquarterb <57384662+realquarterb@users.noreply.github.com>
2020-08-11 00:14:50 +0900

    Create 1.9.cpp

---
+++ 1.9.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+	
+	int sum = 0, val = 50;
+	while (val <= 100) {
+		sum += val;
+		val++;
+	}
+	std::cout << "The sum of the numbers from " << 50 << " to " << 100 << " is " << sum << std::endl;
+}
